618fed0dbe8c84dee5c3958b653c923254963f29
Ricco <Ricco@10.77.32.79>
2012-03-08 13:20:14 +0100

    initiel commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	/**
+	 * @author Ricco Eschelund
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Dette er et del projekt!");
+
+	}
+
+}
